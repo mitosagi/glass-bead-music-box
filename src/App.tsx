8d8900456eb9539efb65cea4d44dc3a785fe9eb2
mitosagi <54105954+mitosagi@users.noreply.github.com>
2020-04-23 05:14:09 +0900

    Fix for type declarations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Matter from "matter-js";
-//eslint-disable-next-line
 import { Chord } from '@tonaljs/tonal';
 import * as Tone from 'tone';
 
@@ -14,29 +13,33 @@ function App() {
   );
 }
 
-class Scene extends React.Component {
+interface ISceneState {
+  chord: string[][];
+  pos: number;
+}
+
+class Scene extends React.Component<Object, ISceneState>  {
   constructor(props: Readonly<{}>) {
     super(props);
-    this.state = { chord: ["C3", "C4", "C5"], pos: 0 };
+    this.state = { chord: [["C3", "C4", "C5"]], pos: 0 };
   }
 
   componentDidMount() {
-    var Engine = Matter.Engine,
+    const Engine = Matter.Engine,
       Render = Matter.Render,
       World = Matter.World,
       Bodies = Matter.Bodies,
       Mouse = Matter.Mouse,
       MouseConstraint = Matter.MouseConstraint;
 
-    var engine = Engine.create({
+    const engine = Engine.create({
       positionIterations: 20
     });
 
     engine.world.gravity.y = 0;
 
-    var render = Render.create({
-      // @ts-ignore
-      element: this.refs.scene,
+    const render = Render.create({
+      element: this.refs.scene as HTMLElement,
       engine: engine,
       options: {
         width: 600,
@@ -45,7 +48,7 @@ class Scene extends React.Component {
       }
     });
 
-    var set = {
+    const set: Matter.IChamferableBodyDefinition = {
       isStatic: true,
       restitution: 1,
       friction: 0,
@@ -67,76 +70,67 @@ class Scene extends React.Component {
     set.label = "ball"
 
     //add balls
-    var ballA = Bodies.circle(100, 100, 50, set);
-    var ballB = Bodies.circle(200, 200, 60, set);
-    var ballC = Bodies.circle(320, 320, 70, set);
-    var ballD = Bodies.circle(450, 450, 80, set);
-    World.add(engine.world, [ballA, ballB, ballC, ballD]);
+    World.add(engine.world, [
+      Bodies.circle(100, 100, 50, set),
+      Bodies.circle(200, 200, 60, set),
+      Bodies.circle(320, 320, 70, set),
+      Bodies.circle(450, 450, 80, set)
+    ]);
 
     // add mouse control
-    var mouse = Mouse.create(render.canvas),
+    const mouse = Mouse.create(render.canvas),
       mouseConstraint = MouseConstraint.create(engine, {
         mouse: mouse,
-        // @ts-ignore
         constraint: {
           stiffness: 0.2,
           render: {
             visible: false
           }
-        }
+        } as Matter.Constraint
       });
 
-    World.add(engine.world, mouseConstraint);
+    World.add(engine.world, mouseConstraint)
+
     //add collision sound
     const synth =
-      // new Synth().toMaster()
       new Tone.Sampler({
         C4: "vibraphone.mp3"
       }, {
-        // @ts-ignore
         baseUrl: "./"
       }).chain(new Tone.Volume(-20), Tone.Destination)
+
+    Matter.Events.on(engine, 'collisionStart', (event) => {
+      [event.pairs.find(pair => pair.bodyA.label.includes("ball") && pair.bodyB.label.includes("ball"))]
+        .map(_ =>
+          this.state.chord[Math.floor(Math.random() * this.state.chord.length)]
+            .map(note =>
+              synth.triggerAttackRelease(note, "1n")))
+    })
+
+    Engine.run(engine)
+    Render.run(render)
+
     this.tick()
     setInterval(
       () => this.tick(),
       70 * 2 / 60 * 1000
     );
-
-    Matter.Events.on(engine, 'collisionStart', (event) => {
-      var pairs = event.pairs;
-      //for (var i = 0; i < pairs.length; i++) {
-      for (var i = 0; i < 1; i++) {
-        var pair = pairs[i];
-        if (pair.bodyA.label.includes("ball") && pair.bodyB.label.includes("ball")) {
-          // @ts-ignore
-          this.state.chord[Math.floor(Math.random() * this.state.chord.length)].forEach((c: any) => {
-            synth.triggerAttackRelease(c, "1n")
-          })
-        }
-      }
-    });
-
-    Engine.run(engine)
-    Render.run(render)
   }
 
   tick = () => {
-    const chords = [
-      "D", "G", "Em", "A",
-      "D", "G", "Em", "A",
-      "Bm", "A", "G", "D",
-      "Em", "A", "D", "D",
-      "D", "G", "A", "Em7",
-      "D", "G", "A", "Em7",]
-    //const chords = ["Am", "Em", "F", "C", "C", "G", "F", "C"]
-    // @ts-ignore
+    //const chords = [
+    // "D", "G", "Em", "A",
+    // "D", "G", "Em", "A",
+    // "Bm", "A", "G", "D",
+    // "Em", "A", "D", "D",
+    // "D", "G", "A", "Em7",
+    // "D", "G", "A", "Em7",]
+    const chords = ["Am", "Em", "F", "C", "C", "G", "F", "C"]
     const notes = Chord.chord(chords[this.state.pos]).notes
     const number = ["3", "4", "5"]
-    // @ts-ignore
-    console.log((this.state.pos + 1) + " " + chords[this.state.pos])
+
     this.setState({
       chord: number.map(num => notes.map(note => note + num)),
-      // @ts-ignore
       pos: (this.state.pos + 1 < chords.length) ? this.state.pos + 1 : 0
     });
   }
